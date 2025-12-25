7787c9c87a732504a9edf8f2ebcc95be1b9c52db
C0deHunger <nachiketstonge@gmail.com>
2025-12-25 19:05:08 +0530

    final working backend

--- NiveshSathi/src/main/java/com/hackathon/NiveshSathi/service/MlService.java
+++ NiveshSathi/src/main/java/com/hackathon/NiveshSathi/service/MlService.java
@@ -2,6 +2,7 @@ package com.hackathon.NiveshSathi.service;
 
 import com.hackathon.NiveshSathi.dto.MlRequest;
 import com.hackathon.NiveshSathi.dto.RecommendationResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,15 @@ public class MlService {
 
     private final RestTemplate restTemplate;
 
+    @Value("${ml.service.url}")
+    private String mlServiceUrl;  // e.g. https://nivesh-sathi-ml-service-1.onrender.com
+
     public MlService(RestTemplateBuilder builder) {
         this.restTemplate = builder.build();
     }
 
     public RecommendationResponse getRecommendation(MlRequest request) {
-
-        String url = "https://nivesh-sathi-ml-service-1.onrender.com/predict";
+        String url = mlServiceUrl + "/predict";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
